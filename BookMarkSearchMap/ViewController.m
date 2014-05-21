45328fd2f6512eb994f1396a98066e84f0e8bc25
MARCUS HSU <kiddkevin01@gmail.com>
2014-05-21 18:57:30 -0400

    update

--- BookMarkSearchMap/ViewController.m
+++ BookMarkSearchMap/ViewController.m
@@ -7,7 +7,6 @@
 //
 @import MapKit;
 #import <COMSMapManager/COMSMapManager.h>
-//#import "AnnotationDelegate.h"
 #import "ViewController.h"
 #import "Annotation.h"
 #import "AnotherViewController.h"
@@ -17,12 +16,11 @@
 @property (strong,nonatomic)CLLocationManager *manager;
 @property (nonatomic) CLLocationCoordinate2D cur;
 
-//@property(nonatomic) MKCoordinateRegion region;
-//@property (nonatomic, strong) NSMutableArray* resultData;
+
+
 @end
 
 @implementation ViewController
-//@synthesize manager,map,region,cur,resultData;
 
 - (void)viewDidLoad
 {
@@ -33,23 +31,6 @@
     
     self.resultList = [[NSMutableArray alloc] init];
     
-    
-    //self.manager.desiredAccuracy = kCLLocationAccuracyBest;
-    //[self.manager startUpdatingLocation];
-    
-    //    region.center.latitude = 40.748647;
-    //    region.center.longitude = -73.984726;
-    //
-    //
-    //    map.mapType = MKMapTypeStandard;
-    //    map.zoomEnabled = YES;
-    //    MKCoordinateSpan span;
-    //    span.latitudeDelta = .1;
-    //    span.longitudeDelta = .1;
-    //    region.span =span;
-    //    [map setRegion:region animated:YES];
-    
-    
 }
 
 -(void)viewDidAppear:(BOOL)animated{
@@ -59,18 +40,12 @@
     
 }
 
-//-(void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error
-//{
-//    NSLog(@"location mananger error : %@", error);
-//}
-
-
 - (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations
 {
     self.cur = ((CLLocation *)locations[0]).coordinate;
     NSLog(@"User location updated, current locaiton:\nlat: %f, lng: %f\n", self.cur.latitude, self.cur.longitude);
     [self.manager stopUpdatingLocation];
-    [GoogleMapManager nearestVenuesForLatLong:self.cur withinRadius:self.radius forQuery:self.name queryType:self.type  googleMapsAPIKey:@" PLEASE_ENTER_YOUR_API_KEY_HERE " searchCompletion:^(NSMutableArray *results)
+    [GoogleMapManager nearestVenuesForLatLong:self.cur withinRadius:self.radius forQuery:self.name queryType:self.type  googleMapsAPIKey:@" PLEASE_ENTER_YOUR_GOOGLE_MAP_API_KEY " searchCompletion:^(NSMutableArray *results)
      {
          if ([results count] != 0) {
              [self.resultList addObjectsFromArray:results];
@@ -104,17 +79,10 @@
                  
                  [self.map addAnnotation:annotation];
                  [self.map showAnnotations:self.map.annotations animated:YES];
-                 //AnnotationDelegate * annotation = [[AnnotationDelegate alloc] initWithLocation:newCoordinate];
-                 // annotation.title = name;
-                 //annotation.subtiltle = vicinity;
-                 
-                 //[resultData addObject:annotation];
-                 
                  
              }
              
-             // Send tasks to UI thread to relfect immediate update of UI
-             //[self performSelectorOnMainThread:@selector(addAnnotationsToMap:) withObject:resultData waitUntilDone:true];
+            
              self.status.text = [NSString stringWithFormat:@"  Here's the results you want !!"];
              
          }else{
@@ -123,59 +91,7 @@
          }
      }];
     
-    
-    
-    //NSLog(@"LocationManager callback : %@", locations);
-    
-    //[self performSearch];
-    //if([locations[0] isKindOfClass:[CLLocation class]]){
-    // [self.manager stopUpdatingLocation];
-    //cur = ((CLLocation *)locations[0]).coordinate;
-    //	region.center.latitude = 40.748647;
-    //	region.center.longitude = -73.984726;
-    
-    //        cur = region.center ;
-    //
-    //        map.mapType = MKMapTypeStandard;
-    //        map.zoomEnabled = YES;
-    //        MKCoordinateSpan span;
-    //        span.latitudeDelta = .1;
-    //        span.longitudeDelta = .1;
-    //        region.span =span;
-    //        [map setRegion:region animated:YES];
-    
-    
-    
-    
-    //}
-    
 }
-/*
- - (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id < MKAnnotation >)annotation{
- //Declare annotation view
- NSLog(@"Calling for pinView for %@", annotation);
- MKPinAnnotationView* aView = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:@"AnnotationDelegate"];
- aView.canShowCallout = YES;
- //aView.centerOffset = CGPointMake(10, -20);
- return aView;
- 
- 
- }
- */
 
-//-(void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation{
-//
-//}
-
-//- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
-//    if([segue.identifier isEqualToString:@"list"])  {
-//        
-//        AnotherViewController *dest = segue.destinationViewController;
-//        
-//        dest.list = [[NSMutableArray alloc] init];
-//        [dest.list addObjectsFromArray:self.resultList];
-//        NSLog(@"list: %@",self.resultList);
-//    }
-//}
-
+    
 @end
